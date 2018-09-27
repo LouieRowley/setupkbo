2cccdb7adacf40a918314fbe57dd295af2bcca36
Louie Rowley <11794034+LouieRowley@users.noreply.github.com>
2018-09-26 20:45:03 -0700

    Update and rename _2_setup.sh to _1_setup.sh

--- _1_setup.sh
+++ _1_setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-echo "install keys"
-sudo apt-key adv --keyserver hkp://p80.POOL.SKS-KEYSERVERS.NET:80 --recv-keys 58118e89f3a912897c070adbf76221572c52609d
-echo "instal docker repo"
-sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 echo "get latest updates"
 sudo apt-get update
+
+echo "install keys"
+sudo apt-key adv --keyserver hkp://p80.POOL.SKS-KEYSERVERS.NET:80 --recv-keys 58118e89f3a912897c070adbf76221572c52609d
+
+echo "instal docker repo"
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+
+echo "get latest updates"
+sudo apt-get update
+
 #echo "show docker as candidate"
 #apt-cache policy docker-engine
 echo "install docker engine and start"
 sudo apt-get install -y docker-engine
 sudo systemctl status docker > status.log
 nano status.log
+
 echo "get toolbox docker"
 sudo docker pull kobotoolbox/kobocat
 
