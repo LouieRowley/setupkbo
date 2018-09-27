aef6ec62888bd2f0873ab5a482cfee6952868c85
Louie Rowley <11794034+LouieRowley@users.noreply.github.com>
2018-09-26 20:47:07 -0700

    Update and rename _1_setup.sh to _1_kobosetup.sh

--- _1_kobosetup.sh
+++ _1_kobosetup.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 echo "install keys"
 sudo apt-key adv --keyserver hkp://p80.POOL.SKS-KEYSERVERS.NET:80 --recv-keys 58118e89f3a912897c070adbf76221572c52609d
 
-echo "instal docker repo"
+echo "install docker repo"
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 
 echo "get latest updates"
@@ -17,7 +17,4 @@ echo "install docker engine and start"
 sudo apt-get install -y docker-engine
 sudo systemctl status docker > status.log
 nano status.log
-
-echo "get toolbox docker"
-sudo docker pull kobotoolbox/kobocat
 
